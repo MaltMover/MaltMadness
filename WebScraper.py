87783a927510f57d3d4cf9f16eb7d2a16cc4e9a7
DNIIBOY <daniel@nettelfield.com>
2023-05-18 22:47:42 +0200

    Get github issues with bs4

--- WebScraper.py
+++ WebScraper.py
@@ -2,7 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class WebScraper:
-    def __init__(self):
-        self.page = requests.get(self.url)
-        self.content = self.page.content
+def get_github_issues() -> list[str]:
+    url = "https://github.com/MaltMover/MaltMadness/issues"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    issues = soup.find_all("a", {"class": "Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title"})
+    return [issue.text.strip() for issue in issues]
+
+
+if __name__ == '__main__':
+    print(get_github_issues())
