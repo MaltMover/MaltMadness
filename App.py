2e170503d72ca983b8a6723207de0a949bfee520
Gustav <gustav@gustavnybro.com>
2023-05-18 21:15:46 +0200

    Amimir failed window exists now

--- App.py
+++ App.py
@@ -62,6 +62,48 @@ class QuizWindow(Window):
             self.option_buttons[i].place(x=100, y=100 + i * 100, width=200, height=50)
 
 
+def closing_popup():
+    # Failed popup window
+    failed_window = Tk()
+    failed_window.geometry("300x300")
+    failed_window.title("Vil du skride?")
+    failed_window.configure(bg="#123456")
+    Label(failed_window, text="Vil du lukke programmet?", font=('Mistral 18 bold')).place(x=50, y=80)
+    Label(failed_window, text="Nå, jamen det må du ikke", font=('Arial 12')).place(x=70, y=120)
+    Label(failed_window, text="Fordi her. Elsker. Vi. At Hygge. :)", font=('Oswald 12 bold')).place(x=0, y=180)
+    Label(failed_window, text="Du skal spille vidre, slayyyyyyy", font=('Arial 12')).place(x=30, y=220)
+    Button(failed_window, text="Tak skatter <3", command=failed_window.destroy).place(x=50, y=260)
+
+
+class FailedWindow(Window):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.configure(bg="#000000")
+
+        self.failed_label = Label(self, text="Du er så dum, men det er okay <3", bg="#000000", fg="#ffffff",
+                                  font=("Oswald", 36))
+        self.retry_button = OptionButton(self, "Prøv igjen", None)
+        self.quit_button = OptionButton(self, "Avslutt", closing_popup)
+        self.drink_button = OptionButton(self, "Drikk", None)
+
+        self.setup()
+
+    def setup(self):
+        self.failed_label.configure(
+            anchor="center",
+            justify="center",
+        )
+        self.drink_button.configure(
+            bg="#FFFF00",
+            fg="#ffffff",
+            font=("Arial", 12)
+        )
+        self.failed_label.place(x=0, y=0, width=1248, height=100)
+        self.drink_button.place(x=100, y=150, width=1000, height=200)
+        self.quit_button.place(x=100, y=500, width=893, height=50)
+        self.retry_button.place(x=100, y=400, width=900, height=50)
+
+
 class OptionButton(Button):
     def __init__(self, parent, text, command, bg="#FF0000", fg="#ffffff", font=("Arial", 12)):
         Button.__init__(self, parent, text=text, command=command, bg=bg, fg=fg, font=font)
