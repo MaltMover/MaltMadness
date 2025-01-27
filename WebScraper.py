fb8f4750266d0cc815ad80707282d2fdb7e02088
DNIIBOY <daniel.nettelfield@gmail.com>
2025-01-27 23:14:14 +0100

    Fix Byggemand bob

--- WebScraper.py
+++ WebScraper.py
@@ -13,8 +13,8 @@ class WebScraper:
         except requests.exceptions.ConnectionError:
             return self.github_issues
         soup = BeautifulSoup(response.text, "html.parser")
-        issues = soup.find_all("a", {"class": "Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title"})
-        self.github_issues = [issue.text.strip() for issue in issues]
+        issues = soup.find_all("h3")
+        self.github_issues = [issue.text.strip() for issue in issues][:-1]
         return self.github_issues
 
 
